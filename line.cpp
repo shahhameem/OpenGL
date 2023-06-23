082d05966544ba0e83b81003cd7cd6a04bf87cf2
hameeshah <muhammad.hameem.shah>
2023-06-23 17:19:39 +0530

    hexagon

--- line.cpp
+++ line.cpp
@@ -1,17 +1,16 @@
-#include <graphics.h>
-#include <windows.h>
+// #include <graphics.h>
 
-int main() {
-	int gd = DETECT, gm;
-	initgraph(&gd, &gm, (char *)"");
-	int j = 0;
-	for(int i = 0; i < getmaxy(); i++) {
-		j %= 10;
-		j++;
-		putpixel(i, 300, j);
-		delay(50);
-	}
-	getch();
-	closegraph();
-	return 0;
-}
+// int main() {
+// 	int gd = DETECT, gm;
+// 	initgraph(&gd, &gm, (char *)"");
+// 	int j = 0;
+// 	for(int i = 0; i < getmaxy(); i++) {
+// 		j %= 10;
+// 		j++;
+// 		putpixel(i, 300, j);
+// 		delay(50);
+// 	}
+// 	getch();
+// 	closegraph();
+// 	return 0;
+// }
