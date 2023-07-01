ed5e68d9d1fd3d6bdc9e69b9c9edd3733692af19
unknown <me.habiba.ayob@gmail.com>
2023-07-01 06:16:24 +0530

    opengl.cpp

--- opengl.cpp
+++ opengl.cpp
@@ -1,28 +1,33 @@
 #include<GL/freeglut.h>
 
-//Program to create an empty Widdow
-void init(){
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);	//Line C
-	glutInitWindowSize(640,480);
-	glutInitWindowPosition(1000,200);
-	glutCreateWindow("Simple Window");
+void init(void){
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glMatrixMode(GL_PROJECTION);
+	gluOrtho2D(0.0,100.0,0,100.0);
 }
 
 void display()
 {
-	glClearColor(1.0,1.0,1.0,0.0);
 	glClear(GL_COLOR_BUFFER_BIT);
-	// gluOrtho2D(0.0,100.0,0,100.0);
+    glColor3f(0.0, 0.0, 0.2);   //set the line color
+	glBegin(GL_POINTS);
+		glVertex2i(50, 50);
+	glEnd();
 	glFlush();
 }
 
 int main(int argc,char **argv)
 {
 	glutInit(&argc,argv);			//Line A
-	init();					//Line B
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);	//Line C
+	glutInitWindowSize(640,480);
+	glutInitWindowPosition(1000,200);
+	glutCreateWindow("Simple Window");
+
+	init();
 	glutDisplayFunc(display);
+
 	glutMainLoop();
-	
 	return 0;
 }
 
